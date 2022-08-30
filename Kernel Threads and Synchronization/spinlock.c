e2da7dcf6a739b07b24364b4affc8a75097f87b1
Somya-Khandelwal <somyakhwal@gmail.com>
2022-08-30 21:24:18 +0530

    Lab0 Done

--- Kernel Threads and Synchronization/spinlock.c
+++ Kernel Threads and Synchronization/spinlock.c
@@ -49,15 +49,6 @@ release(struct spinlock *lk)
   lk->pcs[0] = 0;
   lk->cpu = 0;
 
-  // The xchg serializes, so that reads before release are 
-  // not reordered after it.  The 1996 PentiumPro manual (Volume 3,
-  // 7.2) says reads can be carried out speculatively and in
-  // any order, which implies we need to serialize here.
-  // But the 2007 Intel 64 Architecture Memory Ordering White
-  // Paper says that Intel 64 and IA-32 will not move a load
-  // after a store. So lock->locked = 0 would work here.
-  // The xchg being asm volatile ensures gcc emits it after
-  // the above assignments (and after the critical section).
   xchg(&lk->locked, 0);
 
   popcli();
---
+++ Setup/code/drawtest.c
@@ -0,0 +1,10 @@
+#include "types.h"
+#include "user.h"
+
+int main(int argc, char *argv[]){
+	printf(1, "I am a turtle.\n\n");
+	char turt[500];
+	draw(turt, 500);
+	printf(1, turt);
+	exit();
+}
